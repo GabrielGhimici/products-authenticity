ae250cb6c9f7c0ead1e61314d3b8a806545a81f3
GabrielGhimici <gabriel.ghimici@teads.tv>
2019-08-21 01:30:20 +0300

    search page done on web

--- client/app/root/search-product/search-product.component.ts
+++ client/app/root/search-product/search-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'search-product',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-product.component.scss']
 })
 export class SearchProductComponent implements OnInit {
-
+  @ViewChild('uploadTrigger',  {static: false}) uploadTrigger: ElementRef;
+  public active = false;
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('drop', (event) => {
+      event.preventDefault();
+    });
   }
 
+  upload() {
+    this.uploadTrigger.nativeElement.click();
+  }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event) {
+    event.preventDefault();
+    this.active = true;
+  }
+
+  @HostListener('dragLeave')
+  onDragLeave() {
+    this.active = false;
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(event);
+    this.active = false;
+  }
 }
